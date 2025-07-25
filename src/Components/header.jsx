f6325ee852576ea698ef6283900a5b021ebb7a3f
muhammad uzair <abuzada735@gmail.com>
2025-07-26 03:52:21 +0500

    first commit

---
+++ src/Components/header.jsx
@@ -0,0 +1,12 @@
+const header = () => {
+  return (
+    <>
+      <header className="App-header">
+        <div className="div">Blogify</div>
+        <div className="div"></div>
+      </header>
+    </>
+  );
+};
+
+export default header;
